e7ba51e4da30bae27511c57463ccd2731931f97c
Masi Dawoud <masidawoud@Masis-Air-2.home>
2026-03-23 10:11:42 +0100

    Revert branchPrefix — does not solve branch UI problem

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,7 +1,7 @@
 import { config, singleton, fields } from '@keystatic/core';
 
 export default config({
-  storage: { kind: 'cloud', branchPrefix: 'content/' },
+  storage: { kind: 'cloud' },
   cloud: { project: 'mondzorg/mondzorg-test' },
 
   singletons: {
